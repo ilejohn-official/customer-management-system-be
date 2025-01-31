1816d6be57c0cdd9bc21c88896b708ef40017864
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-01-31 07:51:40 +0100

    set up tests config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,24 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    /**
+     * Setup the test environment.
+     * 
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!file_exists(__DIR__ . '/../.env.testing')) {
+            throw new \Exception('.env.testing file required!!!');
+        }
+    }
 }
